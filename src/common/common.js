8c68c5ca8ce1dfd8583588c4d47ebd633648af1a
tyassusanto <nasusayt@gmail.com>
2023-07-31 01:35:59 +0700

    middlewere

--- src/common/common.js
+++ src/common/common.js
@@ -23,14 +23,25 @@ const auth = (req, res, next) => {
         const secretKey = 'KMZWAY87AA';
         const decoded = jwt.verify(token, secretKey);
         req.username = decoded.username
-        // console.log('decode: ', decoded);
+        req.levelId = decoded.levelId
+        // console.log('decode: ', decoded.levelId);
       } catch(err) {
         return next(createError(403, 'Invalid Token'))
       }
       next()
 }
 
+const isAdmin = (req, res, next) => {
+  const levelId = req.levelId;
+  if (levelId === '31538b4b-8942-497f-8066-33db39262d63') {
+    next();
+  } else {
+    return next(createError(403, 'Hanya Administrator yang dapat menghapus'))
+  }
+};
+
   module.exports = { 
     response,
     auth,
+    isAdmin
  }
