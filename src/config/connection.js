9e955e1fa3f67c9bb82390ae5ec2df430a1aab51
tyassusanto <nasusayt@gmail.com>
2023-07-31 08:13:14 +0700

    add dotenv

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,10 @@
-// require('dotenv').config()
+require('dotenv').config()
 const mysql = require('mysql2')
 const connection = mysql.createConnection({
-    host : 'localhost', 
-    user : 'root',
-    password : '8karakteR#',
-    database : 'jsm'
+    host : process.env.DB_HOST, 
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
 })
 
 module.exports = connection
