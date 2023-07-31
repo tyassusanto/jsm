a038db6e2f1fa78a83a11b57ad766e094148f07a
tyassusanto <nasusayt@gmail.com>
2023-07-31 08:13:30 +0700

    auth get

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const adminController = require('../controller/admin');
 const common = require('../common/common')
 
-route.get('/get', adminController.getUsers)
+route.get('/get', common.auth, adminController.getUsers)
 route.post('/addemployee', adminController.addEmployee) 
 route.delete('/delete/:id', common.auth, common.isAdmin, adminController.deleteUsers)
 
